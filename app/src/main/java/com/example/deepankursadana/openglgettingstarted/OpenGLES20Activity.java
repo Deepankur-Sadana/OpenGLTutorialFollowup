e9363cc692d0e0af6dc755c4f6a4caa791814e4e
Deepankur Sadana <deepankursadana@Deepankurss-MacBook-Pro.local>
2017-02-22 12:55:48 +0530

    building the envirnonment

---
+++ app/src/main/java/com/example/deepankursadana/openglgettingstarted/OpenGLES20Activity.java
@@ -0,0 +1,24 @@
+package com.example.deepankursadana.openglgettingstarted;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+/**
+ * Created by deepankursadana on 22/02/17.
+ */
+
+public class OpenGLES20Activity extends Activity {
+
+    private GLSurfaceView mGLView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
+    }
+}
